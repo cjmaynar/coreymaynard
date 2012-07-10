fc27506a845fdd9205d39a8a4cb8cd2f0a38fa93
Corey Maynard <me@coreymaynard.com>
2012-07-10 03:55:24 +0000

    Add math validation for comments - humans only

--- apps/blog/forms.py
+++ apps/blog/forms.py
@@ -13,6 +13,7 @@ class CommentForm(ModelForm):
     author_email = forms.CharField(label="Your Email", required=False)
     comment = forms.CharField(widget=forms.Textarea())
     extra = forms.CharField(required=False)
+    math = forms.CharField(label="What's 5 + 2?")
 
     def clean_extra(self):
         '''If there is contents in the extra field, don't allow the comment'''
@@ -21,6 +22,14 @@ class CommentForm(ModelForm):
             raise forms.ValidationError(u'Shouldn\'t be doing this...')
 
         return extra
+
+    def clean_math(self):
+        '''Verify that someone is can do basic math'''
+        math = self.cleaned_data['math']
+        if math != 7:
+            raise forms.ValidationError(u'5 + 2 = 7')
+
+        return math
 
     class Meta:
         model = Comment
