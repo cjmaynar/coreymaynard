7a7b8b10dcf674a40502c9320d12a39c9d601530
Corey Maynard <me@coreymaynard.com>
2026-04-25 17:28:45 -0400

    Add coreymaynard.com to allowed hosts

--- settings/prod.py
+++ settings/prod.py
@@ -6,9 +6,12 @@ from .common import *
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 PRODUCTION = True
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', "")
 
-ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME', '')]
+ALLOWED_HOSTS = [
+    os.environ.get("CUSTOM_DOMAIN", "coreymaynard.com"),
+    os.environ.get('RENDER_EXTERNAL_HOSTNAME', '')
+]
 
 USER_NAME = 'cjmaynar'
 NAME = 'Corey Maynard'
